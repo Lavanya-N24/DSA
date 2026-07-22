823c9a6131801026a7aee7502d331210d014f3c3
Lavanya N M <lavanyanm75@gmail.com>
2026-07-23 00:11:42 +0530

    first commit

---
+++ LargestArray.java
@@ -0,0 +1,19 @@
+public class LargestArray {
+    public static int findLargest(int[] arr) {
+       
+        int largest = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > largest) {
+                largest = arr[i];
+            }
+        }
+        return largest;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {3, 5, 7, 2, 8};
+        int largest = findLargest(array);
+        System.out.println("The largest element in the array is: " + largest);
+    }
+    
+}
