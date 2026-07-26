fd0d3d0bc4be51da5d7a906defb6e611173524f5
Lavanya N M <lavanyanm75@gmail.com>
2026-07-26 21:39:15 +0530

    PatternsOne

--- PatternsOne.java
+++ PatternsOne.java
@@ -1,10 +1,21 @@
+//Pattern 1-4*4 star pattern
+import java.util.Scanner;
+
 public class PatternsOne {
     public static void main(String[] args) {
-        for(int i=0;i<4;i++){
-            for(int j=0;j<4;j++){
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter the number of rows and columns");
+        int n = sc.nextInt();
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
                 System.out.print(" * ");
             }
             System.out.println();
         }
+        
+        sc.close();
     }
 }
+
