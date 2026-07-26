d68e1485e7ce776f840864c62d90b15c9436b496
Lavanya N M <lavanyanm75@gmail.com>
2026-07-26 23:47:12 +0530

    PatternsOne

--- patternsSeven.java
+++ patternsSeven.java
@@ -8,13 +8,13 @@ public class patternsSeven {
       
         for(int i=0;i<n;i++){
             for(int j=1;j<=n-i-1;j++){
-                System.out.print("  ");
+                System.out.print("   ");
             }
                for(int j=1;j<=2*i+1;j++){
                 System.out.print(" * ");
             }
                for(int j=1;j<=n-i-1;j++){
-                System.out.print("  ");
+                System.out.print("   ");
             }
             System.out.println();
         }
