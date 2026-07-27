c694f57fb3fb5de49b8dd6b739315d9a53470f60
Lavanya N M <lavanyanm75@gmail.com>
2026-07-28 00:08:47 +0530

    all code

---
+++ patternThirteen.java
@@ -0,0 +1,25 @@
+
+  // 1      1
+// 12    21
+// 123  321
+// 12344321
+import java.util.Scanner;
+public class patternThirteen {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n = sc.nextInt();
+        int num=1;
+       for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            System.out.print(num+" ");
+            num=num+1;
+        }
+        
+          System.out.println();      
+        
+        }
+       
+        sc.close();
+       }
+    }
