61db33dc414c131813fdfe6f3b17ec7a3d8e31fb
Lavanya N M <lavanyanm75@gmail.com>
2026-07-28 00:30:43 +0530

    all code

--- patternFifteen.java
+++ patternFifteen.java
@@ -1,19 +1,22 @@
 
-// A 
-// A B 
-// A B C 
-// A B C D 
+
+// 1 
+// 2 3 
+// 4 5 6 
+// 7 8 9 10 
 import java.util.Scanner;
-public class patternFourteen {
+public class patternFifteen {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number");
         int n = sc.nextInt();
-        int num=1;
-       for(int i=0;i<n;i++){
-        for(char ch='A';ch<='A'+i;ch++){
-            System.out.print(ch+" ");
+    
+       for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            System.out.print((char)('A'+j-1)+" ");
+           
         }
+        
           System.out.println();      
         
         }
