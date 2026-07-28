cc5fc2774a9adba345032ef29f550505fa93f17c
Lavanya N M <lavanyanm75@gmail.com>
2026-07-28 22:20:58 +0530

    one commit

--- patternSixteen.java
+++ patternSixteen.java
@@ -1,19 +1,20 @@
-// A B C D 
-// A B C 
-// A B 
 // A 
+// B B
+// C C C
+// D D D D
 
 
 import java.util.Scanner;
-public class patternFifteen {
+public class patternSixteen {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number");
         int n = sc.nextInt();
     
-       for(int i=1;i<=n;i++){
-        for(int j=1;j<=n-i+1;j++){
-            System.out.print((char)('A'+j-1)+" ");
+       for(int i=0;i<n;i++){
+        char ch=(char)('A'+i);
+        for(int j=0;j<=i;j++){
+            System.out.print(ch+" ");
            
         }
         
@@ -24,3 +25,5 @@ public class patternFifteen {
         sc.close();
        }
     }
+
+
