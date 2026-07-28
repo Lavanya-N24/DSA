aa35553a570c3c5f46a4df9ca016994c6341350b
Lavanya N M <lavanyanm75@gmail.com>
2026-07-28 22:59:34 +0530

    one commit

---
+++ patternSeventeen.java
@@ -0,0 +1,48 @@
+
+   // A 
+// B B
+// C C C
+// D D D D
+
+
+import java.util.Scanner;
+public class patternSeventeen {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n = sc.nextInt();
+    
+       for(int i=0;i<n;i++){
+        for(int j=1;j<=n-i-1;j++){
+            System.out.print(" ");
+           
+        }
+        char ch=(char)('A');
+        int breakpoint=i;
+         for(int j=1;j<=2*i+1;j++){
+            System.out.print(ch);
+            if(j<=breakpoint){
+                ch++;
+            }
+            else{
+                ch--;
+            }
+           
+        }
+        for(int j=1;j<=n-i-1;j++){
+ System.out.print(" ");
+           
+        
+               
+        
+        }
+        System.out.println(); 
+    }
+        sc.close();
+       
+    }
+}
+
+
+
+
