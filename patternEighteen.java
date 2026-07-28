2cd036f0455f469bc20913032400995fa279e660
Lavanya N M <lavanyanm75@gmail.com>
2026-07-28 23:25:35 +0530

    one commit

---
+++ patternEighteen.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+public class patternEighteen {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n = sc.nextInt();
+    
+       for(int i=0;i<n;i++){
+        for(char ch=(char)('E'-i);ch<='E';ch++){
+            System.out.print(ch+" ");
+        }
+        System.out.println(); 
+    }
+        sc.close();
+       
+    }
+}
+
+
+
+
+
+
