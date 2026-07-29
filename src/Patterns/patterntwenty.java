12dd6f08c7acc64bd15f423829a7c772b9269874
Lavanya N M <lavanyanm75@gmail.com>
2026-07-29 21:46:13 +0530

    one commit

--- src/Patterns/patterntwenty.java
+++ src/Patterns/patterntwenty.java
@@ -6,13 +6,13 @@ public class patterntwenty {
         int n = sc.nextInt();
     
        for(int i=0;i<n;i++){
-     for(int j=1;j<=i;j++){
+     for(int j=0;j<=i;j++){
         System.out.print("*");
      }
      for(int j=1;j<=2*(n-i)-2;j++){
         System.out.print(" ");
      }
-      for(int j=1;j<=i;j++){
+      for(int j=0;j<=i;j++){
         System.out.print("*");
      }
       System.out.println();
