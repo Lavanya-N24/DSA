9a3659f0e4af1ce8fa223e2397b92b4ac669d292
Lavanya N M <lavanyanm75@gmail.com>
2026-07-29 22:12:03 +0530

    one commit

--- src/Patterns/patternTwentyOne.java
+++ src/Patterns/patternTwentyOne.java
@@ -7,34 +7,25 @@ public class patternTwentyOne {
         int n = sc.nextInt();
     
        for(int i=0;i<n;i++){
-     for(int j=1;j<=n-i;j++){
-        System.out.print("*");
-     }
-     for(int j=1;j<2*i;j++){
-        System.out.print(" ");
-     }
-      for(int j=1;j<=n-i;j++){
-        System.out.print("*");
-     }
-      System.out.println();
+     for(int j=0;j<n;j++){
+        if(i==0||i==n-1||j==0||j==n-1){
+            System.out.print("*");
+        }
+        else{
+            System.out.print(" ");
+        }
+        
+       
        }
-    for(int i=0;i<=n;i++){
-        for(int j=0;j<=i;j++){
-        System.out.print("*");
-     }
-      for(int j=0;j<=2-(2*i);j++){
-        System.out.print(" ");
-     }
-        for(int j=0;j<=i;j++){
-        System.out.print("*");
-     }
         System.out.println();
-       }
+    }
 
       sc.close();
         }
         
          
     }
+
+
 
 
