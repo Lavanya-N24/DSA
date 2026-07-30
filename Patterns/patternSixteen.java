28d62a926e0587acae06bd240984910a9b00e743
Lavanya N M <lavanyanm75@gmail.com>
2026-07-30 23:40:14 +0530

    all code

--- Patterns/patternSixteen.java
+++ Patterns/patternSixteen.java
@@ -1,4 +1,4 @@
-package Patterns;
+
 // A 
 // B B 
 // C C C 
@@ -6,25 +6,24 @@ package Patterns;
 // E E E E E
 
 import java.util.Scanner;
+
 public class patternSixteen {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number");
-        int n = sc.nextInt();
-    
-       for(int i=0;i<n;i++){
-        char ch=(char)('A'+i);
-        for(int j=0;j<=i;j++){
-            System.out.print(ch+" ");
-           
-        }
-        
-          System.out.println();      
-        
-        }
-       
-        sc.close();
-       }
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the number");
+    int n = sc.nextInt();
+
+    for (int i = 0; i < n; i++) {
+      char ch = (char) ('A' + i);
+      for (int j = 0; j <= i; j++) {
+        System.out.print(ch + " ");
+
+      }
+
+      System.out.println();
+
     }
 
-
+    sc.close();
+  }
+}
