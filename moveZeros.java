f288d005818c75e864fddda4cd725683c6d3e4e0
Lavanya N M <lavanyanm75@gmail.com>
2026-07-31 21:50:05 +0530

    all commit

--- moveZeros.java
+++ moveZeros.java
@@ -1,27 +1,29 @@
 
-    class Solution {
+public class moveZeros {
+  static class Solution {
     public void moveZeroes(int[] nums) {
-      int j=0;
-      for(int i=0;i<nums.length;i++){
-        if(nums[i]!=0){
-            nums[j]=nums[i];
-            j++;
+      int j = 0;
+      for (int i = 0; i < nums.length; i++) {
+        if (nums[i] != 0) {
+          nums[j] = nums[i];
+          j++;
         }
-      } 
-      while(j<nums.length){
-        nums[j]=0;
+      }
+      while (j < nums.length) {
+        nums[j] = 0;
         j++;
       }
       System.out.println(j);
     }
-}
-public class moveZeros {
+  }
+
   public static void main(String[] args) {
     Solution s = new Solution();
-    int[] nums = {0,1,0,3,12};
+    int[] nums = { 0, 1, 0, 3, 12 };
     s.moveZeroes(nums);
-    for(int i=0;i<nums.length;i++){
-      System.out.print(nums[i]+" ");
+    for (int i = 0; i < nums.length; i++) {
+      System.out.print(nums[i] + " ");
     }
   }
 }
+
--- removeDuplicatesFromSortedArray.java
+++ removeDuplicatesFromSortedArray.java
@@ -1,24 +1,21 @@
 
-    class Solution {
-    public int removeDuplicates(int[] nums) {
-        int j=1;
-        for(int i=1;i<nums.length;i++){
-              if(nums[i]!=nums[i-1]){
-                 nums[j]=nums[i]; 
-                j++;
-               
-            }
-           
-        }
-        if(nums.length==0){
-            return 0;
-        }
-    
-         return j;
-    
-}
-}
 public class removeDuplicatesFromSortedArray {
+    static class Solution {
+        public int removeDuplicates(int[] nums) {
+            int j = 1;
+            for (int i = 1; i < nums.length; i++) {
+                if (nums[i] != nums[i-1]) {
+                    nums[j] = nums[i]; 
+                    j++;
+                }
+            }
+            if (nums.length == 0) {
+                return 0;
+            }
+            return j;
+        }
+    }
+
     public static void main(String[] args) {
         Solution s = new Solution();
         int[] nums = {1,1,2};
@@ -29,3 +26,4 @@ public class removeDuplicatesFromSortedArray {
         }
     }
 }
+
