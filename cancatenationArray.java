da06542a37e6d17fcab50ada9bb68fa7c73677e4
Lavanya N M <lavanyanm75@gmail.com>
2026-08-01 22:35:21 +0530

    one  commit

---
+++ cancatenationArray.java
@@ -0,0 +1,26 @@
+
+    class Solution3 {
+    public int[] getConcatenation(int[] nums) {
+        int ans[]=new int[2*nums.length];
+        for(int i=0;i<nums.length;i++){
+            ans[i]=nums[i];
+            ans[i+nums.length]=nums[i];
+           
+        }
+       
+         return ans;
+    }
+   
+}
+public class cancatenationArray {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        Solution3 solution = new Solution3();
+        int[] result = solution.getConcatenation(nums);
+
+        System.out.print("Concatenated array: ");
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+    }
+}
