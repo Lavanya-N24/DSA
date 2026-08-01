378ce8f9ef2aaa7132acfa53405341f76c027674
Lavanya N M <lavanyanm75@gmail.com>
2026-08-01 23:26:38 +0530

    one commit

---
+++ armstrongNumber.java
@@ -0,0 +1,32 @@
+
+    class Solution6 {
+    public boolean isArmstrong(int n) {
+        int temp=n;
+int sum=0;
+while(n>0){
+int lastdigit=n%10;
+sum=sum+(lastdigit*lastdigit*lastdigit);
+n=n/10;
+}
+if(temp==sum){
+    return true;
+}
+else{
+    return false;
+}
+
+    }
+}
+public class armstrongNumber {
+    public static void main(String[] args) {
+        int n = 153;
+        Solution6 solution = new Solution6();
+        boolean isArmstrong = solution.isArmstrong(n);
+        if (isArmstrong) {
+            System.out.println(n + " is an Armstrong number.");
+        } else {
+            System.out.println(n + " is not an Armstrong number.");
+        }
+    }
+     
+}
