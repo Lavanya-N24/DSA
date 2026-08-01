057ba08a29e274b91df7c6ef2aa3f26edc2bf8d4
Lavanya N M <lavanyanm75@gmail.com>
2026-08-01 22:43:36 +0530

    one commit

--- BasicMaths/divisorNumber.java
+++ BasicMaths/divisorNumber.java
@@ -2,20 +2,29 @@ package BasicMaths;
 
 public class divisorNumber {
     public static void main(String[] args) {
-        int n ;
+        int n = 36;
         int count = 0;
+
         for (int i = 1; i <= n; i++) {
             if (n % i == 0) {
                 count++;
             }
-            int arr[] = new int[count];
-int index=0;
-        } for (int i = 1; i <= n; i++) {
+        }
+
+        int[] arr = new int[count];
+        int index = 0;
+
+        for (int i = 1; i <= n; i++) {
             if (n % i == 0) {
                 arr[index++] = i;
             }
         }
+
+        System.out.print("Divisors of " + n + ": ");
+        for (int divisor : arr) {
+            System.out.print(divisor + " ");
+        }
+        System.out.println();
         System.out.println("Number of divisors of " + n + " is: " + count);
     }
-    
 }
