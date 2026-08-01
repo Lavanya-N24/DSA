f963af4a0e04e5576ed949b1c2bc369f4f44edd4
Lavanya N M <lavanyanm75@gmail.com>
2026-08-01 22:25:26 +0530

    all commit

---
+++ containsDuplicates.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+
+
+    class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer,Integer>map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(nums[i])){
+                return true;
+            }
+            map.put(nums[i],i);
+        }
+        return false;
+    }
+}
+public class containsDuplicates {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, 1}; // Example input array
+        Solution solution = new Solution();
+        boolean hasDuplicates = solution.containsDuplicate(nums);
+        System.out.println("Contains duplicates: " + hasDuplicates);
+    }
+}
