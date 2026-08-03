1f5e16dcb3bc52f9062ae78dc2d7fec6a3eab515
Lavanya N M <lavanyanm75@gmail.com>
2026-08-03 21:47:39 +0530

    one commit

---
+++ Recursion/printNameNTimes.java
@@ -0,0 +1,22 @@
+package Recursion;
+
+public class printNameNTimes {
+    public static void printName(String name, int n) {
+        if (n <= 0) {
+            return;
+        }
+        System.out.println(name);
+        printName(name, n - 1);
+    }
+
+    public static void main(String[] args) {
+        String name = "lavanya";
+        int n = 5;
+
+        printName(name, n);
+    }
+}
+
+
+    
+
