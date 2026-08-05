958a61f10d6a90f51353326284340ca12e2cc3db
Lavanya N M <lavanyanm75@gmail.com>
2026-08-05 22:08:37 +0530

    one commit

---
+++ reverseArray.java
@@ -0,0 +1,22 @@
+public class reverseArray {
+    public void reverse(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+            // Swap elements at start and end
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        reverseArray ra = new reverseArray();
+        ra.reverse(arr);
+        System.out.println("Reversed array: " + java.util.Arrays.toString(arr));
+    }
+    }
+
