ed364a3a471d06e0f62a03e36babdb983d4dc550
Lavanya N M <lavanyanm75@gmail.com>
2026-08-05 23:44:13 +0530

    one commit

---
+++ Fibonacci.java
@@ -0,0 +1,13 @@
+public class Fibonacci {
+    public static int fibonacci(int n) {
+         n=10;
+         int first=0;
+         int second=1;
+       for(int i=2;i<n;i++){
+      int next=first+second;
+      System.out.print(next+" ");
+      first=second;
+      second=next;
+       }
+     return second;
+    }}
