8aa5bd0613b9f87c34750ad18a09c2d3a6c11500
Lavanya N M <lavanyanm75@gmail.com>
2026-08-09 01:07:29 +0530

    sorting

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,34 @@
+package Sorting;
+
+public class BubbleSort {
+  
+
+    static void bubbleSort(int[] arr) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+
+                    // Swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {13, 46, 24, 52, 20, 9};
+
+        bubbleSort(arr);
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
+
