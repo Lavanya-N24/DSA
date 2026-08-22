150876f6b6294369cd426d7c972707dc59e746bb
Lavanya N M <lavanyanm75@gmail.com>
2026-08-23 00:12:31 +0530

    Time: 0 ms (100%), Space: 43 MB (62.87%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,44 +1,33 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-         List<Integer> result = new ArrayList<>();
-
-        int top = 0;
-        int bottom = matrix.length - 1;
-        int left = 0;
-        int right = matrix[0].length - 1;
-
-        while (top <= bottom && left <= right) {
-
-            // 1. Left → Right
-            for (int j = left; j <= right; j++) {
+        
+        List<Integer>result=new ArrayList<>();
+        int left=0;
+        int right=matrix[0].length-1;
+        int top=0;
+        int bottom=matrix.length-1;
+        while(left<=right && top<=bottom){
+            for(int j=left;j<=right;j++){
                 result.add(matrix[top][j]);
             }
             top++;
-
-            // 2. Top → Bottom
-            for (int i = top; i <= bottom; i++) {
+            for(int i=top;i<=bottom;i++){
                 result.add(matrix[i][right]);
             }
             right--;
-
-            // 3. Right → Left
-            if (top <= bottom) {
-                for (int j = right; j >= left; j--) {
-                    result.add(matrix[bottom][j]);
-                }
-                bottom--;
+            if(top<=bottom){
+            for(int j=right;j>=left;j--){
+                result.add(matrix[bottom][j]);
             }
-
-            // 4. Bottom → Top
-            if (left <= right) {
-                for (int i = bottom; i >= top; i--) {
-                    result.add(matrix[i][left]);
-                }
-                left++;
+            bottom--;
+            }
+            if(left<=right){
+            for(int i=bottom;i>=top;i--){
+                result.add(matrix[i][left]);
+            }
+            left++;
             }
         }
-
-        return result;
-    
+    return result;
     }
 }
